80cca08ea3976458eb6fa1d27411f8ed5cdefa1d
891680996 <891680996@qq.com>
2017-03-18 11:07:11 +0800

    one file

---
+++ example.sv
@@ -0,0 +1,19 @@
+/*==========================================================*/
+
+//generate year month day signals output to display moudle
+
+/*==========================================================*/
+module example (
+    input clk,    // Clock
+    input clk_en, // Clock Enable
+    input rst_n,  // Asynchronous reset active low
+    
+);
+
+
+
+
+
+/***********************************************************/
+endmodule
+/*==========================================================*/
